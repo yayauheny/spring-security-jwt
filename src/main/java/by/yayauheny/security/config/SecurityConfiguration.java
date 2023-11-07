b6b9fc75771768bd6721c90307045da4c7c6ab89
yayauheny <work.evles@gmail.com>
2023-11-07 16:11:00 +0300

    implement oauth2

--- src/main/java/by/yayauheny/security/config/SecurityConfiguration.java
+++ src/main/java/by/yayauheny/security/config/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package by.yayauheny.security.config;
 
 import by.yayauheny.security.repository.UserRepository;
+import by.yayauheny.security.service.AuthenticationService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
@@ -14,12 +15,20 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Set;
 
 @Configuration
 @EnableWebSecurity
@@ -29,13 +38,20 @@ public class SecurityConfiguration {
     private static final String[] WHITE_LIST_URL = {"/api/auth/**", "/api/demo/**"};
     private final JwtAuthenticationFilter jwtFilter;
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationService authenticationService;
 
     public SecurityConfiguration(
             @Lazy
             JwtAuthenticationFilter jwtFilter,
-            UserRepository userRepository) {
+            UserRepository userRepository,
+            PasswordEncoder passwordEncoder,
+            @Lazy
+            AuthenticationService authenticationService) {
         this.jwtFilter = jwtFilter;
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.authenticationService = authenticationService;
     }
 
     @Bean
@@ -47,18 +63,54 @@ public class SecurityConfiguration {
                                 .permitAll()
                                 .anyRequest().authenticated()
                 )
+                .formLogin(Customizer.withDefaults())
+//                .oauth2Login(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider())
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
-                .oauth2Login(Customizer.withDefaults())
+                .oauth2Login(config ->
+                        config.userInfoEndpoint(userInfo -> userInfo.oidcUserService(oidcUserService())))
                 .build();
+    }
+
+    private OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService() {
+        return userRequest -> {
+            String email = userRequest.getIdToken().getClaim("email");
+//            try {
+            UserDetails userDetails = userDetailsService().loadUserByUsername(email);
+            DefaultOidcUser oidcUser = new DefaultOidcUser(userDetails.getAuthorities(), userRequest.getIdToken());
+//            } catch (UsernameNotFoundException e) {
+//                String fullname = userRequest.getIdToken().getClaim("name");
+//                String[] nameArray = fullname.split(" ");
+//                String firstname = nameArray[0];
+//                String lastname = nameArray[1];
+//
+//                AuthenticationResponse authenticationResponse = authenticationService.register(
+//                        new RegisterRequest(
+//                                firstname,
+//                                lastname,
+//                                email,
+//                                "defaultPass",
+//                                Role.USER.name()
+//                        )
+//                );
+//            }
+
+            Set<Method> userDetailsMethods = Set.of(UserDetails.class.getMethods());
+
+            return (OidcUser) Proxy.newProxyInstance(SecurityConfiguration.class.getClassLoader(),
+                    new Class[]{UserDetails.class, OidcUser.class},
+                    (proxy, method, args) -> userDetailsMethods.contains(method)
+                            ? method.invoke(userDetails, args)
+                            : method.invoke(oidcUser, args));
+        };
     }
 
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
         authProvider.setUserDetailsService(userDetailsService());
-        authProvider.setPasswordEncoder(passwordEncoder());
+        authProvider.setPasswordEncoder(passwordEncoder);
         return authProvider;
     }
 
@@ -67,10 +119,6 @@ public class SecurityConfiguration {
         return config.getAuthenticationManager();
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     @Bean
     public UserDetailsService userDetailsService() {
---
+++ src/main/java/by/yayauheny/security/controller/AuthController.java
@@ -0,0 +1,16 @@
+package by.yayauheny.security.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class AuthController {
+
+    @GetMapping("/oauth2/code/google")
+    public ResponseEntity<String> sayHello(){
+        return ResponseEntity.ok("Hello from auth controller!");
+    }
+}
